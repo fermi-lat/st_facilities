05fae93e5ac01d8520c97b8a2afe4c74540b684e
peachey <>
2004-09-07 14:15:27 +0000

    Add some facilities for interactions with environment variables and dealing with paths.

--- src/test/test.cxx
+++ src/test/test.cxx
@@ -3,7 +3,7 @@
  * @brief Test program for st_facilities
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/st_facilities/src/test/test.cxx,v 1.1.1.1 2004/08/25 04:55:03 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/st_facilities/src/test/test.cxx,v 1.2 2004/08/26 17:02:24 jchiang Exp $
  */
 
 #ifdef TRAP_FPE
@@ -21,6 +21,7 @@
 #include "st_facilities/dgaus8.h"
 #include "PowerLaw.h"
 
+#include "st_facilities/Env.h"
 #include "st_facilities/Util.h"
 
 using namespace st_facilities;
@@ -34,6 +35,9 @@ class st_facilitiesTests : public CppUnit::TestFixture {
    CPPUNIT_TEST(test_Util_readLines);
    CPPUNIT_TEST(test_Util_expectedException);
    CPPUNIT_TEST(test_Util_resolve_fits_files);
+   CPPUNIT_TEST(test_Env_appendNames);
+   CPPUNIT_TEST(test_Env_expandEnvVar);
+   CPPUNIT_TEST(test_Env_getDataDir);
 
    CPPUNIT_TEST_SUITE_END();
 
@@ -47,6 +51,9 @@ public:
    void test_Util_readLines();
    void test_Util_expectedException();
    void test_Util_resolve_fits_files();
+   void test_Env_appendNames();
+   void test_Env_expandEnvVar();
+   void test_Env_getDataDir();
 
 private:
 
@@ -57,9 +64,9 @@ private:
 void st_facilitiesTests::setUp() {
    m_filename = "test_file.txt";
    std::ofstream file(m_filename.c_str());
-   file << "line 0\n" 
-        << "line 1\n" 
-        << "line 2\n" 
+   file << "line 0\n"
+        << "line 1\n"
+        << "line 2\n"
         << "#line 3\n"
         << " "
         << "\n";
@@ -85,7 +92,7 @@ void st_facilitiesTests::test_dgaus8() {
    dgaus8_(&power_law, &xmin, &xmax, &err, &result, &ierr);
    double true_value;
    if (powerLaw.index() != 1.) {
-      true_value = powerLaw.prefactor()*(pow(xmax, powerLaw.index() + 1.) 
+      true_value = powerLaw.prefactor()*(pow(xmax, powerLaw.index() + 1.)
                                          - pow(xmin, powerLaw.index() + 1.))
                                          /(powerLaw.index() + 1.);
    } else {
@@ -122,12 +129,153 @@ void st_facilitiesTests::test_Util_resolve_fits_files() {
    Util::resolve_fits_files(m_filename, lines);
    CPPUNIT_ASSERT(lines.size() == 3);
 }
-   
+
+void st_facilitiesTests::test_Env_appendNames() {
+   std::string tmp_str;
+   std::string::size_type pos;
+   std::string seg1 = "seg1-name";
+   std::string seg2 = "seg2-name";
+
+   tmp_str = Env::appendPath("", "");
+   CPPUNIT_ASSERT(tmp_str.empty());
+
+   tmp_str = Env::appendPath(seg1, "");
+   CPPUNIT_ASSERT(tmp_str == seg1);
+
+   tmp_str = Env::appendPath("", seg2);
+   CPPUNIT_ASSERT(tmp_str == seg2);
+
+   tmp_str = Env::appendPath(seg1, seg2);
+   pos = tmp_str.find(seg1);
+   CPPUNIT_ASSERT(0 == pos);
+   pos = tmp_str.find(seg2);
+   CPPUNIT_ASSERT(tmp_str.size() - seg2.size() == pos);
+}
+
+void st_facilitiesTests::test_Env_expandEnvVar() {
+   std::string to_expand;
+   std::string expanded;
+
+   to_expand = "$STTEST";
+   Env::expandEnvVar(to_expand, expanded);
+   CPPUNIT_ASSERT("sttest" == expanded);
+   expanded.clear();
+
+   to_expand = "${STTEST}";
+   Env::expandEnvVar(to_expand, expanded);
+   CPPUNIT_ASSERT(expanded == "sttest");
+   expanded.clear();
+
+   to_expand = "$(STTEST)";
+   Env::expandEnvVar(to_expand, expanded);
+   CPPUNIT_ASSERT(expanded == "sttest");
+   expanded.clear();
+
+   to_expand = "$(STTEST) filler $STTEST";
+   Env::expandEnvVar(to_expand, expanded);
+   CPPUNIT_ASSERT(expanded == "sttest filler sttest");
+   expanded.clear();
+
+   to_expand = "filler$(STTEST)filler$STTEST";
+   Env::expandEnvVar(to_expand, expanded);
+   CPPUNIT_ASSERT(expanded == "fillersttestfillersttest");
+   expanded.clear();
+
+   to_expand = "$(STTEST)}";
+   Env::expandEnvVar(to_expand, expanded);
+   CPPUNIT_ASSERT(expanded == "sttest}");
+   expanded.clear();
+
+   to_expand = "$STTEST filler";
+   Env::expandEnvVar(to_expand, expanded);
+   CPPUNIT_ASSERT(expanded == "sttest filler");
+   expanded.clear();
+
+   to_expand = "$";
+   try {
+      Env::expandEnvVar(to_expand, expanded);
+      CPPUNIT_ASSERT(false);
+   } catch (const std::exception &) {
+      CPPUNIT_ASSERT(expanded == to_expand);
+   }
+   expanded.clear();
+
+   to_expand = "$$";
+   try {
+      Env::expandEnvVar(to_expand, expanded);
+      CPPUNIT_ASSERT(false);
+   } catch (const std::exception &) {
+      CPPUNIT_ASSERT(expanded == to_expand);
+   }
+   expanded.clear();
+
+   to_expand = "$STTEST_run_on";
+   try {
+      Env::expandEnvVar(to_expand, expanded);
+      CPPUNIT_ASSERT(false);
+   } catch (const std::exception &) {
+      CPPUNIT_ASSERT(expanded == to_expand);
+   }
+   expanded.clear();
+
+   to_expand = "${STTEST:f}";
+   try {
+      Env::expandEnvVar(to_expand, expanded);
+      CPPUNIT_ASSERT(false);
+   } catch (const std::exception &) {
+      CPPUNIT_ASSERT(expanded == to_expand);
+   }
+   expanded.clear();
+
+   to_expand = "prefix${STTEST";
+   try {
+      Env::expandEnvVar(to_expand, expanded);
+      CPPUNIT_ASSERT(false);
+   } catch (const std::exception &) {
+      CPPUNIT_ASSERT(expanded == to_expand);
+   }
+   expanded.clear();
+}
+
+void st_facilitiesTests::test_Env_getDataDir() {
+   // For comparison of output, need local and install areas.
+   std::string install_area;
+   std::string local_area = Env::getEnv("ST_FACILITIESROOT");
+
+   try {
+      // For comparison purposes, expand the install area env variable.
+      std::string install_area = Env::getEnv("DATADIR");
+   } catch (const std::exception &) {
+      // That's OK.
+   }
+
+   std::string data_dir;
+   // First test: an invalid package identifier.
+   try {
+      data_dir = Env::getDataDir("invalid_pkg");
+      // If the above line did not throw an exception, the data_dir must be the install area.
+      CPPUNIT_ASSERT(!install_area.empty() && install_area == data_dir);
+   } catch (const std::exception &) {
+      // Expected only if install area env variable is not set.
+      CPPUNIT_ASSERT(install_area.empty());
+   }
+
+   data_dir.erase();
+   try {
+      data_dir = Env::getDataDir("st_facilities");
+      // If the above line did not throw an exception, the data_dir must be the local area.
+      CPPUNIT_ASSERT(0 == data_dir.find(local_area));
+   } catch (const std::exception &) {
+      // Unexpected.
+      CPPUNIT_ASSERT(false);
+   }
+}
+
 int main() {
    CppUnit::TextTestRunner runner;
-   
+
    runner.addTest(st_facilitiesTests::suite());
-    
+
    bool result = runner.run();
    if (result) {
       return 0;
