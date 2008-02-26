8c562c51c4aaf42afe58a5f068c114e43994d22f
glastrm <>
2008-02-26 16:20:52 +0000

    Add lib dependencies when creating a library

--- st_facilitiesLib.py
+++ st_facilitiesLib.py
@@ -1,9 +1,12 @@
+#$Id$
 def generate(env, **kw):
-	env.Tool('addLibrary', library = ['st_facilities', 'g2c'], package = 'st_facilities')
+	if not kw.get('depsOnly',0):
+		env.Tool('addLibrary', library = ['st_facilities'])
 	env.Tool('astroLib')
 	env.Tool('tipLib')
 	env.Tool('facilitiesLib')
 	env.Tool('addLibrary', library = env['cfitsioLibs'])
+	env.Tool('addLibrary', library = env['f2cLibs'])
 
 def exists(env):
 	return 1
