e54bd536d51ed0c9ac5838649b2dfdc131784d9e
jchiang <>
2009-04-02 19:10:35 +0000

    add static function to update the FILENAME keyword in the primary HDU

--- st_facilities/FitsUtil.h
+++ st_facilities/FitsUtil.h
@@ -3,7 +3,7 @@
  * @brief Static functions for accessing data from FITS files.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/st_facilities/st_facilities/FitsUtil.h,v 1.2 2004/10/04 18:30:45 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/st_facilities/st_facilities/FitsUtil.h,v 1.3 2007/10/20 15:50:21 jchiang Exp $
  */
 
 #ifndef st_facilities_FitsUtil_h
@@ -17,7 +17,7 @@ namespace st_facilities {
 /**
  * @class FitsUtil
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/st_facilities/st_facilities/FitsUtil.h,v 1.2 2004/10/04 18:30:45 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/st_facilities/st_facilities/FitsUtil.h,v 1.3 2007/10/20 15:50:21 jchiang Exp $
  */
 
 class FitsUtil {
@@ -47,6 +47,9 @@ public:
 
    /// Write checksum and datasum information for all HDUs in a FITS file.
    static void writeChecksums(const std::string & filename);
+
+   /// Write FILENAME keyword to primary header
+   static void writeFilename(const std::string & filename);
 
    /// Interface to fits_copy_file.
    static void fcopy(std::string infilename,
