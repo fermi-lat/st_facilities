07ecbcb945759a09538fe92eeee67c8289ad2e84
jrb <>
2012-12-11 21:18:41 +0000

    fix include paths when building GR on Windows

--- st_facilitiesLib.py
+++ st_facilitiesLib.py
@@ -1,12 +1,22 @@
-#$Id$
+#$Id: st_facilitiesLib.py,v 1.2 2008/02/26 16:20:52 glastrm Exp $
 def generate(env, **kw):
 	if not kw.get('depsOnly',0):
-		env.Tool('addLibrary', library = ['st_facilities'])
+	    env.Tool('addLibrary', library = ['st_facilities'])
+
+	    if env['PLATFORM'] == 'win32'and env.get('CONTAINERNAME','')=='GlastRelease':
+		env.Tool('findPkgPath', package = 'st_facilities') 
+
+
 	env.Tool('astroLib')
 	env.Tool('tipLib')
 	env.Tool('facilitiesLib')
 	env.Tool('addLibrary', library = env['cfitsioLibs'])
 	env.Tool('addLibrary', library = env['f2cLibs'])
+
+	if env['PLATFORM'] == 'win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+
+	    env.Tool('findPkgPath', package = 'facilities') 
+
 
 def exists(env):
 	return 1
