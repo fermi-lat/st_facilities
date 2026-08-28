1605e234afec35ea8ffe22394a7c95c8e6eeb6d8
jasercion <jasercion.gwu@gmail.com>
2026-08-28 14:41:32 -0400

    update dgaus8 function call

--- src/dgaus8.c
+++ src/dgaus8.c
@@ -25,8 +25,8 @@ static integer c__72 = 72;
 static logical c_true = TRUE_;
 
 /* DECK DGAUS8 */
-/* Subroutine */ int dgaus8_(D_fp fun, doublereal *a, doublereal *b, 
-	doublereal *err, doublereal *ans, integer *ierr)
+/* Subroutine */ 
+doublereal dgaus8_(doublereal (*fun)(doublereal *), doublereal *a, doublereal *b, doublereal *err, integer *ierr)	
 {
     /* Initialized data */
 
